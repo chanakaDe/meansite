ebc375855225d8b0f327e47abe6321ff96b1ce8e
Lior Kesos <lior@linnovate.net>
2014-04-07 17:17:42 +0300

    applied animation on page change, started restructure

--- public/js/controllers/docs.js
+++ public/js/controllers/docs.js
@@ -1,15 +1,23 @@
 'use strict';
 
 angular.module('mean.system').controller('DocsController',
-  ['$scope', '$window','$location','$anchorScroll',
-  function( $scope, $window, $location, $anchorScroll) {
-//	$scope.height = $window.innerHeight ;
-//  console.log($window.inner)
-	  $scope.view = 'getting-started';
-	  $scope.changeView = function(name, anchor){
-      $location.hash(anchor);
+  ['$scope', '$window','$location','$anchorScroll','$timeout',
+  function( $scope, $window, $location, $anchorScroll,$timeout) {
+    $scope.resetSlide = function(){
+      console.log('reset',$scope.view,$scope.slide);
+      $scope.slide = false;
+    };
+
+    $scope.changeView = function(name, anchor){
+      console.log('in',$scope.view,$scope.slide);
+      $scope.slide = false;
       $scope.view = name;
-      $anchorScroll();
-	  }
+      $timeout(function(){
+        $scope.slide=true;
+        console.log('here');
+        $location.hash(anchor);
+        $anchorScroll();
+      },600);
+    };
   }
 ]);
