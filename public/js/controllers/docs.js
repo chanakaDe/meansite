e74f5295c5b4f05ef1827450dff1a161ee754e2c
Lior Kesos <lior@linnovate.net>
2014-04-09 03:07:25 +0300

    Restructured docs, added technologies content

--- public/js/controllers/docs.js
+++ public/js/controllers/docs.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mean.system').controller('DocsController',
-  ['$scope', '$window','$location','$anchorScroll','$timeout',
+  ['$scope','$window','$location','$anchorScroll','$timeout',
   function( $scope, $window, $location, $anchorScroll,$timeout) {
     $scope.resetSlide = function(){
       console.log('reset',$scope.view,$scope.slide);
@@ -9,15 +9,17 @@ angular.module('mean.system').controller('DocsController',
     };
 
     $scope.changeView = function(name, anchor){
-      console.log('in',$scope.view,$scope.slide);
-      $scope.slide = false;
+      console.log('in',$scope.view, $scope.slide, 'name',name);
+      $scope.slide = false;//when with timeout initialize as false
       $scope.view = name;
-      $timeout(function(){
+
+      $timeout(function(name,anchor){
         $scope.slide=true;
         console.log('here');
-        $location.hash(anchor);
-        $anchorScroll();
+      /*  $location.hash(anchor);
+        $anchorScroll(); - buggy scope wise*/
       },300);
+
     };
   }
 ]);
