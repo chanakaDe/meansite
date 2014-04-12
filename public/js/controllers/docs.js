5dcbc6c3686e5f9aee5e23597a7d4864e303b3d7
ehud shahak <udisun@gmail.com>
2014-04-12 11:44:21 +0300

    Added design to the docs sidebar and fixed some bugs on homepage

--- public/js/controllers/docs.js
+++ public/js/controllers/docs.js
@@ -36,24 +36,17 @@ angular.module('mean.system').controller('DocsController', ['$scope', '$window',
     }
 
 
-    //////
-    $scope.resetSlide = function() {
-      console.log('reset', $scope.view, $scope.slide);
-      $scope.slide = false;
-    };
+    $("#docs .nav a").on('click', function (e) {
 
-    $scope.changeView = function(name, anchor) {
-      console.log('in', $scope.view, $scope.slide, 'name', name);
-      $scope.slide = false; //when with timeout initialize as false
-      $scope.view = name;
+      // prevent default anchor click behavior
+      e.preventDefault();
 
-      $timeout(function(name, anchor) {
-        $scope.slide = true;
-        console.log('here');
-        /*  $location.hash(anchor);
-        $anchorScroll(); - buggy scope wise*/
-      }, 300);
 
-    };
+      // animate
+      $('html, body').animate({
+        scrollTop: $(this.hash).offset().top - 60
+      }, 300, "easeOutExpo");
+
+    });
   }
 ]);
