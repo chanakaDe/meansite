8fd50742270f853002c3de9a7b111ad69e1f80cf
Drew Fyock <fyockm@gmail.com>
2014-05-02 09:54:26 -0400

    add latest release info to homepage

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -3,8 +3,11 @@
 angular.module('mean.system').controller('IndexController', ['$scope', '$http', 'Global',
     function($scope, $http, Global) {
         $scope.global = Global;
-        $http.jsonp('https://api.github.com/repos/linnovate/mean?callback=JSON_CALLBACK').success(function(data) {
-            $scope.githubData = data;
+        $http.jsonp('https://api.github.com/repos/linnovate/mean?callback=JSON_CALLBACK').success(function(repo) {
+            $scope.githubData = repo.data;
+						$http.jsonp(repo.data.tags_url + '?callback=JSON_CALLBACK').success(function(tags) {
+							$scope.githubData.release = tags.data[0].name;
+						});
         });
     }
 ]);
